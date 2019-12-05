09ad7d791f526f1a332dcbf66ed261181bea1f41
SElioenai <44786318+SElioenai@users.noreply.github.com>
2019-12-04 21:14:03 -0300

    Atividade clima

--- hellospringboot/target/classes/import.sql
+++ hellospringboot/target/classes/import.sql
@@ -1,33 +1,27 @@
+INSERT INTO tb_semana(id_semana,dia_semana) VALUES (1,'Segunda-Feira')
+INSERT INTO tb_semana(id_semana,dia_semana) VALUES (2,'Terça-Feira')
+INSERT INTO tb_semana(id_semana,dia_semana) VALUES (3,'Quarta-Feira')
+INSERT INTO tb_semana(id_semana,dia_semana) VALUES (4,'Quinta-Feira')
+INSERT INTO tb_semana(id_semana,dia_semana) VALUES (5,'Sexta-Feira')
+INSERT INTO tb_semana(id_semana,dia_semana) VALUES (6,'Sábado')
+INSERT INTO tb_semana(id_semana,dia_semana) VALUES (7,'Domingo')
 
-	insert into previsoes (min, max, hum, des, data, hora) values (27.0, 33.0, 40.0, 'alta chance de chuva','15/10/2019', 25.54);
-	insert into dias (semana) values ('Sexta');
-	insert into cidades (nome,latitude, longitude) values ('São Paulo', 100.55, 75.29 );
-	
-	
-	update previsoes set dia = 1 where id = 1;
-	update previsoes set cidade_id = 1 where id = 1;
-	update dias set previsao = 1 where id = 1;
-	
-	
-	insert into previsoes (min, max, hum, des, data, hora) values ( 20.0, 30.0, 40.0, 'alta chance de chuva forte','17/10/2019', 17.40);
-	insert into dias (semana) values ('Sexta');
-	insert into cidades (nome,latitude, longitude) values ('Rio' , 90.24, 35.89);
+INSERT INTO tb_cidade(id_cidade,nome_cidade,lat_cidade,lon_cidade) VALUES (1,'Cajamar',12.0,-2.0)
+INSERT INTO tb_cidade(id_cidade,nome_cidade,lat_cidade,lon_cidade) VALUES (2,'São Paulo',0,-5.0)
+INSERT INTO tb_cidade(id_cidade,nome_cidade,lat_cidade,lon_cidade) VALUES (3,'São Bernardo',39448.0,-25.0)
+INSERT INTO tb_cidade(id_cidade,nome_cidade,lat_cidade,lon_cidade) VALUES (4,'Guarulhos',7338.0,-4335.0)
+INSERT INTO tb_cidade(id_cidade,nome_cidade,lat_cidade,lon_cidade) VALUES (5,'Osasco',3226.20,-5224.20)
+INSERT INTO tb_cidade(id_cidade,nome_cidade,lat_cidade,lon_cidade) VALUES (6,'Rio de Janeiro',1345.30,-5437.0)
+INSERT INTO tb_cidade(id_cidade,nome_cidade,lat_cidade,lon_cidade) VALUES (7,'Curitiba',38.57,-347.120)
 
-	
-	
-	update previsoes set dia = 2 where id = 2;
-	update previsoes set cidade_id = 2 where id = 2;
-	update dias set previsao = 2 where id = 2;
+insert into tb_tempo (id_tempo,id_cidade,id_semana,tempmin_tempo,tempmax_tempo,humidade_tempo,datahora_tempo,desc_tempo) values (1,1,1,21.4,27.2,56,'1996-08-07','');
+insert into tb_tempo (id_tempo,id_cidade,id_semana,tempmin_tempo,tempmax_tempo,humidade_tempo,datahora_tempo,desc_tempo) values (2,2,2,21.4,27.2,56,'1996-08-07','');
+insert into tb_tempo (id_tempo,id_cidade,id_semana,tempmin_tempo,tempmax_tempo,humidade_tempo,datahora_tempo,desc_tempo) values (3,3,3,20.6,27.8,0,'1996-08-07','');
+insert into tb_tempo (id_tempo,id_cidade,id_semana,tempmin_tempo,tempmax_tempo,humidade_tempo,datahora_tempo,desc_tempo) values (4,4,4,20.8,27.8,34,'1996-08-07','');
+insert into tb_tempo (id_tempo,id_cidade,id_semana,tempmin_tempo,tempmax_tempo,humidade_tempo,datahora_tempo,desc_tempo) values (5,5,5,20.1,27.7,56,'1996-08-07','');
+insert into tb_tempo (id_tempo,id_cidade,id_semana,tempmin_tempo,tempmax_tempo,humidade_tempo,datahora_tempo,desc_tempo) values (6,6,6,20.4,25.5,24,'1996-08-07','');
+insert into tb_tempo (id_tempo,id_cidade,id_semana,tempmin_tempo,tempmax_tempo,humidade_tempo,datahora_tempo,desc_tempo) values (7,7,7,22.3,27.2,56,CURRENT_DATE(),'');
 
-	insert into previsoes (min, max, hum, des, data, hora) values (18.0, 31.0, 63.0, 'pouca chance de chuva','16/9/2019', 10.15);
-	insert into dias (semana) values ('Domingo');
-	insert into cidades (nome,latitude, longitude) values ('São camilo', 90.55, 75.88);
 
-	
-	update previsoes set dia = 3 where id = 3;
-	update previsoes set cidade_id = 3 where id = 3;
-	update dias set previsao = 3 where id = 3;
 
---adicione um usuário
-insert into usuario (id, login, senha) values (1, 'admin', 'admin');
-
+insert into usuario (id, login, senha) values (1, 'admin', 'admin')
